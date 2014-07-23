a2d77cb854790a70e2d5a5ac731142c0445507cb
Becky Carella <becarella@gmail.com>
2014-07-23 12:44:19 -0400

    wrong default in comment

--- lib/generators/cursor/templates/cursor_config.rb
+++ lib/generators/cursor/templates/cursor_config.rb
@@ -1,7 +1,7 @@
 Cursor.configure do |config|
   # config.default_per_page = 25
   # config.max_per_page = nil
-  # config.page_method_name = :page_before
+  # config.page_method_name = :page
   # config.before_param_name = :before
   # config.after_param_name = :after
 end
