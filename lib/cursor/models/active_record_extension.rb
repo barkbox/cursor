933cdf26d686e7f6f4218266ac1b8a28798927b3
Cris Kim <cris@criskim.com>
2016-03-15 16:28:42 -0400

    remove alias_chain_method

--- lib/cursor/models/active_record_extension.rb
+++ lib/cursor/models/active_record_extension.rb
@@ -1,17 +1,19 @@
 require 'cursor/models/active_record_model_extension'
 
 module Cursor
+
+  module InheritedOverride
+    def inherited(kls)
+      inherited_without_cursor kls
+      kls.send(:include, Cursor::ActiveRecordModelExtension) if kls.superclass == ::ActiveRecord::Base
+    end
+  end
+
   module ActiveRecordExtension
     extend ActiveSupport::Concern
     included do
       # Future subclasses will pick up the model extension
-      class << self
-        def inherited_with_cursor(kls) #:nodoc:
-          inherited_without_cursor kls
-          kls.send(:include, Cursor::ActiveRecordModelExtension) if kls.superclass == ::ActiveRecord::Base
-        end
-        alias_method_chain :inherited, :cursor
-      end
+      self.prepend Cursor::InheritedOverride
 
       # Existing subclasses pick up the model extension as well
       self.descendants.each do |kls|
