b322cc694525d0a7cf6bb86dcb5dc6e59a4df6d3
Erik Sälgström Peterson <erik@barkbox.com>
2019-03-28 16:15:00 -0400

    Set default for page method to cursor

--- lib/cursor/config.rb
+++ lib/cursor/config.rb
@@ -6,22 +6,33 @@ module Cursor
   #     config.default_per_page = 10
   #   end
   def self.configure(&block)
-    yield @config ||= Cursor::Configuration.new
+    yield config
   end
 
   # Global settings for Cursor
   def self.config
-    @config
+    @config ||= Cursor::Configuration.new
   end
 
   # need a Class for 3.0
   class Configuration #:nodoc:
     include ActiveSupport::Configurable
-    config_accessor :default_per_page
-    config_accessor :max_per_page
-    config_accessor :page_method_name
-    config_accessor :before_param_name
-    config_accessor :after_param_name
+
+    config_accessor :default_per_page do
+      25
+    end
+    config_accessor :max_per_page do
+      nil
+    end
+    config_accessor :page_method_name do
+      :cursor
+    end
+    config_accessor :before_param_name do
+      :before
+    end
+    config_accessor :after_param_name do
+      :after
+    end
 
     def before_param_name
       config.before_param_name.respond_to?(:call) ? config.before_param_name.call : config.before_param_name
@@ -30,25 +41,5 @@ module Cursor
     def after_param_name
       config.after_param_name.respond_to?(:call) ? config.after_param_name.call : config.after_param_name
     end
-
-
-    # define param_name writer (copied from AS::Configurable)
-    writer, line = 'def before_param_name=(value); config.before_param_name = value; end', __LINE__
-    singleton_class.class_eval writer, __FILE__, line
-    class_eval writer, __FILE__, line
-
-    writer, line = 'def after_param_name=(value); config.after_param_name = value; end', __LINE__
-    singleton_class.class_eval writer, __FILE__, line
-    class_eval writer, __FILE__, line
-
-  end
-
-  # this is ugly. why can't we pass the default value to config_accessor...?
-  configure do |config|
-    config.default_per_page = 25
-    config.max_per_page = nil
-    config.page_method_name = :page
-    config.before_param_name = :before
-    config.after_param_name = :after
   end
 end
