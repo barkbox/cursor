822c098d91eb34f0556afd972464f7960d13bc0a
Erik Sälgström Peterson <erik@barkbox.com>
2019-05-03 13:29:44 -0400

    Update ARE to use prepend

--- lib/cursor/models/active_record_extension.rb
+++ lib/cursor/models/active_record_extension.rb
@@ -1,17 +1,17 @@
 require 'cursor/models/active_record_model_extension'
 
+module CursorInheritance
+  def inherited(kls)
+    super kls
+    kls.send(:include, Cursor::ActiveRecordModelExtension) if kls.superclass == ::ActiveRecord::Base
+  end
+end
+
 module Cursor
   module ActiveRecordExtension
     extend ActiveSupport::Concern
     included do
-      # Future subclasses will pick up the model extension
-      class << self
-        def inherited_with_cursor(kls) #:nodoc:
-          inherited_without_cursor kls
-          kls.send(:include, Cursor::ActiveRecordModelExtension) if kls.superclass == ::ActiveRecord::Base
-        end
-        alias_method_chain :inherited, :cursor
-      end
+      prepend CursorInheritance
 
       # Existing subclasses pick up the model extension as well
       self.descendants.each do |kls|
